2e7e60bcea60c53ef9eb16cd4cde68e193c00ad1
Gaurav-17 <64305413+Gaurav-17@users.noreply.github.com>
2023-12-12 17:56:46 -0500

    Update config.tf

--- prod/network/config.tf
+++ prod/network/config.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "acs730-week3-gjpatel3"                                        // Bucket where to SAVE Terraform State
+    bucket = "acs730-week3-gjpatel3"                                  // Bucket where to SAVE Terraform State
     key    = "assign2/prod/network/terraform.tfstate" // Object name in the bucket to SAVE Terraform State
     region = "us-east-1"                                                    // Region where bucket is created
   }
